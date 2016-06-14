578652cb4311d757d123d747a48d95c00599371c
Milo Davis <milo@basistech.com>
2016-06-14 12:16:02 -0400

    Adds post merge hook

---
+++ rm-current-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm ~/.emacs
+rm ~/.bash_profile
+rm ~/.bashrc
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ln -s `pwd`/post-merge .git/hooks/post-merge
 ln -s `pwd`/emacs ~/.emacs
 ln -s `pwd`/bash_profile ~/.bash_profile
 ln -s `pwd`/bashrc ~/.bashrc
